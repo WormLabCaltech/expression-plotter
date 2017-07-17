0a55f219ef68d7217eeb6e73cfb5571e6b19932a
Joseph Min <kmin@caltech.edu>
2017-07-17 16:30:45 -0700

    q value calculation & heatmap

--- src/expression_plotter.py
+++ src/expression_plotter.py
@@ -7,6 +7,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import itertools as it
+import matplotlib
 
 # TODO: not finished
 def calculate_stats(df_data):
@@ -122,7 +123,7 @@ def calculate_pvalues(boot_deltas, obs_deltas, bootstraps):
     obs_deltas  --- (dictionary) of observed deltas
 
     Returns:
-    pvalues --- (dictionary) of pandas dataframes of p values                          and mutant
+    pvalues --- (dictionary) of pandas dataframes of p values
     """
     # initialize hash
     pvalues = {}
@@ -148,7 +149,7 @@ def calculate_pvalues(boot_deltas, obs_deltas, bootstraps):
             # directly computed.
             # can only say p-value < 1/bootstraps
             if delta < sorted_array[0] or delta > sorted_array[-1]:
-                matrix[pair[0]][pair[1]] = '<' + str(1/bootstraps)
+                matrix[pair[0]][pair[1]] = 0.0
                 print('P-value for ' + measurement + ' ' + pair[0] + ' vs '\
                             + pair[1] + ' could not be accurately calculated.')
             else:
@@ -167,6 +168,7 @@ def calculate_pvalues(boot_deltas, obs_deltas, bootstraps):
                     matrix[pair[0]][pair[1]] = pvalue
 
         # assign matrix to hash
+        matrix = matrix.astype(float)
         pvalues[measurement] = matrix
     return pvalues
 
@@ -194,7 +196,7 @@ def make_empty_dataframe(rows, cols, row_labels, col_labels):
     return matrix
 
 # TODO: work in progress...
-def plot_pvalue_heatmaps(pvalues, threshold):
+def plot_qvalue_heatmaps(pvalues, threshold, **kwargs):
     """
     Plots the pvalues in separate heatmaps. (for each measurement)
 
@@ -205,14 +207,98 @@ def plot_pvalue_heatmaps(pvalues, threshold):
     Returns:
 
     """
-    pvalues['exp3'].fillna(0, inplace=True)
-    print(pvalues['exp3'])
-    array = pvalues['exp3'].values.astype(float)
-    print(len(array))
-    mask = np.in1d(array, 0).reshape(array.shape)
-    ax = sns.heatmap(pvalues['exp3'], cmap='GnBu_r', mask=mask)
-    ax.invert_xaxis()
-    plt.show()
+    title = kwargs.pop('title', '')
+
+    for i, measurement in enumerate(pvalues):
+        print(pvalues[measurement])
+        array = pvalues[measurement].values.astype(float)
+        mask = np.nan_to_num(array) > threshold
+        mask_nan = np.isnan(array)
+        print(mask_nan)
+        plt.figure(i)
+        values = -pvalues[measurement].apply(np.log)
+        bootstrap_n = 3 # TODO: replace this with non-hardcoded param
+        values = values.replace(np.inf, bootstrap_n)
+        vmax = bootstrap_n
+        ax = sns.heatmap(-pvalues[measurement].apply(np.log), cmap='magma', mask=mask,\
+                            vmin=0.0, vmax=vmax)
+        sns.heatmap(-pvalues[measurement].fillna(0).apply(np.log), cmap=matplotlib.colors.ListedColormap(['white']), mask=~mask_nan,\
+                            )
+
+        ax.xaxis.tick_top()
+        ax.invert_yaxis()
+        plt.yticks(rotation='horizontal')
+        plt.xticks(rotation=45)
+        plt.plot()
+        plt.savefig(title + '_' + measurement + '.svg')
+
+def plot_boxplot(data, threshold, **kwargs):
+    """
+    """
+    # data = data.groupby('Genotype')
+    # plt.figure(10)
+    # ax = sns.boxplot(data['exp1'])
+    # plt.show()
+
+def benjamin_hochberg_stepup(pvalues, bootstraps):
+    """
+    Given a list of p-values, apply FDR correction and return the q values.
+
+    Params:
+    pvalues --- (dictionary) of pandas dataframes of p values
+
+    Returns:
+    qvalues --- (dictionary) of pandas dataframes of q values
+    """
+    print(pvalues)
+    qvalues = {}
+    for measurement in pvalues:
+        # flatten 2d array to 1d and convert uncertainties
+        flat = pvalues[measurement].values.flatten()
+        flat[flat == -1] = 1/bootstraps
+        print(flat)
+
+        # sorted pvalues with respective original idices
+        sort = np.sort(flat)
+        idx = np.argsort(flat)
+
+        # pvalues w/o nan values
+        no_nan = ~np.isnan(sort)
+        sort_no_nan = sort[no_nan]
+        idx_no_nan = idx[no_nan]
+
+        print(sort_no_nan)
+        print(idx_no_nan)
+
+        # empty list for qvalues
+        qvalues_sorted = [np.nan] * len(sort_no_nan)
+        prev_q = 0 # store previous q value
+
+        # begin BH step up
+        for i, p in enumerate(sort_no_nan):
+            q = len(sort_no_nan) / (i+1) * p # calculate the q_value for the current point
+            q = min(q, 1) # if q >1, make it == 1
+            q = max(q, prev_q) # preserve monotonicity
+            qvalues_sorted[i] = q # store q value
+            prev_q = q # update the previous q value
+        # end BH step up
+        print(qvalues_sorted)
+
+        qvals = [np.nan] * len(sort)
+        for index, q in zip(idx_no_nan, qvalues_sorted):
+            qvals[index] = q
+
+        # reshape 1d array to dimensions of original 2d array
+        qvals = np.reshape(qvals, pvalues[measurement].shape)
+
+        # row & column labels
+        labels = pvalues[measurement].index.values
+
+        # construct pandas dataframe from data and labels
+        qvals = pd.DataFrame(data=qvals, index=labels, columns=labels)
+        qvalues[measurement] = qvals
+
+    return qvalues
 
 
 if __name__ == '__main__':
@@ -247,7 +333,7 @@ if __name__ == '__main__':
     csv_path = args.csv_data
     title = args.title
     bootstraps = args.b
-    qval = args.q
+    threshold = args.q
 
     df_data = pd.read_csv(csv_path) # read csv data
 
@@ -277,8 +363,11 @@ if __name__ == '__main__':
     # calculate pvalues
     pvalues = calculate_pvalues(boot_deltas, obs_deltas, bootstraps)
 
-    # plot pvalues
-    plot_pvalue_heatmaps(pvalues, 0.05)
+    # calculate q_values
+    qvalues = benjamin_hochberg_stepup(pvalues, bootstraps)
 
+    # plot qvalues
+    plot_qvalue_heatmaps(qvalues, threshold, title=title)
 
-    # plot_bootstraps(boot_deltas, obs_deltas)
+    # plot boxplot
+    plot_boxplot(df_data, threshold, title=title)
